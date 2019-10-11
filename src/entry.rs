639e0ad5581e1e75714f5b7fff3dd599b55c42a2
LCC <lcc@lcc.com>
2019-10-11 17:28:56 +0200

    Added some documentation

--- src/entry.rs
+++ src/entry.rs
@@ -10,6 +10,8 @@ use rand::rngs::{ThreadRng};
 
 const PREFIX_SIZE: usize = 3;
 
+///This class will contain the Prefix of our Markov chain. You can think
+///of it as the memory of the graph so far. 
 #[derive(Clone, Debug, Hash)]
 pub struct Prefix
 {
@@ -25,6 +27,8 @@ impl Prefix {
         }
     }
 
+    ///Update the Prefix with a new (last) word. Very useful
+    ///to update it with a recently-generated word
     pub fn push_new(&mut self, word: String) {
         for i in 0..PREFIX_SIZE-1 {
             self.entries[i] = self.entries[i+1].clone();
@@ -47,6 +51,10 @@ impl PartialEq for Prefix {
 
 impl Eq for Prefix {}
 
+///Structure with the possible followups of any given
+///Prefix. It has two entries, with the word, and their
+///probability of being the next one. Obviously,
+///the sum of all probabilities must should be 1.
 #[derive(Clone, Debug)]
 pub struct FollowUp{
     pub possibilities: HashMap<String, f64>
@@ -58,6 +66,8 @@ impl FollowUp{
         FollowUp{possibilities: HashMap::new()}
     }
 
+    /// Update the probabilities to be sure they 
+    /// sum to one
     pub fn normalize(mut self) {
 
         let total = self.possibilities.values().fold(0.0, |a, b| a + b);
@@ -68,6 +78,9 @@ impl FollowUp{
 
     }
 
+    ///Given a number chosen from a distribution, choses a
+    ///random choice with weights dictated by the second 
+    ///entry in the tuple.
     pub fn choose_next(&self, random_val: f64) -> String {
 
         let mut accum = 0.0;
@@ -85,7 +98,10 @@ impl FollowUp{
     }
 }
 
-
+///Main class of our program. This will create a graph with the Prefix->FollowUps using the text
+///input.
+///
+///For convenience, it has an internal state due to Random Number Generator. 
 #[derive(Clone, Debug)]
 pub struct MarkovData {
     pub data: HashMap<Prefix, FollowUp>,
@@ -96,15 +112,20 @@ impl MarkovData {
 
     pub fn new(text: &String) -> Self{
 
+        //Make sure all parts of the text are in lower case
         let mut loc_text = text.clone();
         loc_text.make_ascii_lowercase();
 
         let mut data: HashMap<Prefix, FollowUp> = HashMap::new();
+        
+        //Split text into words
         let splitted: Vec<&str> = loc_text.split_whitespace().collect();
+
+        //As there is no follow up for the last few words, we are not going to learn 
+        //what could follow them
         let len_c = splitted.len() - PREFIX_SIZE;
 
         for i in 0..len_c {
-            // let p = Prefix::new([splitted[i].to_string(), splitted[i+1].to_string()]);
             let mut words: [String; PREFIX_SIZE] = Default::default();
             for idx in 0..PREFIX_SIZE {
                 words[idx] = splitted[i+idx].to_string();
@@ -117,6 +138,7 @@ impl MarkovData {
             *count += 1.0;
         }
 
+        //Normalize the followups to each Prefix to be sure they sum to one.
         for fu in data.values_mut() {
 
             let total = fu.possibilities.values().fold(0.0, |a, b| a + b);
@@ -131,11 +153,14 @@ impl MarkovData {
 
     }
 
+    ///Choose next word given a Prefix
     pub fn choose_next(&mut self, p: &Prefix) -> String {
         let d = &self.data[p];
         d.choose_next(self.rng.gen_range(0.0, 1.0))
     }
 
+    ///Randomlu choose one of the prefixes present. This is useful
+    ///as a seed for the text. 
     pub fn choose_seed(&mut self) -> Prefix {
         let ran = self.rng.gen_range(0.0, 1.0);
         let each_weight = 1.0/self.data.len() as f64;
